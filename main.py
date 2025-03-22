4a0845e56109919dee499c7340df2248f49ba338
Eduard Bolma <edaurd.bolma@gmail.com>
2025-03-22 15:25:24 +0100

    Task 4 is completed

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import src.task_1 as task_1
 import src.task_2 as task_2
-import task_3
-import task_4
+import src.task_3 as task_3
+import src.task_4 as task_4
+from colorama import Fore, Style
 
 def main():
     task_1.main()
     task_2.main()
     task_3.main()
+    
+    print('')
+    print(Fore.GREEN + '=== TASK 4 ===' + Style.RESET_ALL)
+    print('')
+    
     task_4.main()
+    
+    print('')
+    print(Fore.RED + '=== END TASK 4 ===' + Style.RESET_ALL)
+    print('')
 
 if __name__ == '__main__':
     main()
--- task_4.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print('task 4')
-
-if __name__ == '__main__':
-    main()
